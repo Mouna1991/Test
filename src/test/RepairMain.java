1b8e90f84be05fccfbf6f73d734330a420929cd9
Mouna Hammoudi <mouna@cse.unl.edu>
2016-01-04 16:21:42 -0600

    commit 7

--- src/test/RepairMain.java
+++ src/test/RepairMain.java
@@ -1,5 +1,6 @@
 package test;
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
@@ -23,80 +24,104 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 public class RepairMain {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		// TODO Auto-generated method stub
 		
-//		
-//		String input = null;
-//		System.out.println("Input the path of the test case"); 
-//		try{
-//			BufferedReader br = 
-//	                      new BufferedReader(new InputStreamReader(System.in));
-//				
-//			input=br.readLine(); 
-//				
-//			if(input!=null){
-//				System.out.println(input);
-//				
-//			}
-//			
-//			String s =""; 
-//			Charset charset = Charset.forName("US-ASCII");
-//			Path file=Paths.get(input); 
-//			try (BufferedReader reader = Files.newBufferedReader(file, charset)) {
-//			    String line = null;
-//				FileWriter fw = new FileWriter("test.java");
-//
-//			    while ((line = reader.readLine()) != null) {
-//			       
-//					fw.write(line);
-//					fw.write("\n");
-//					
-//			      
-//			    }
-//			    fw.close();
-//			  
-//
-//			  
-//			} catch (IOException x) {
-//			    System.err.format("IOException: %s%n", x);
-//			}
-//			
-//				
-//		}catch(IOException io){
-//			io.printStackTrace();
-//		}	
-//		
-//		
-//	          
-//		File source = new File(input);
-//		File dest = new File("/Users/mouna/Documents/Research/test/src/test/test.java");
-//		 
-//		String path="/Users/mouna/Documents/Research/test/src/test/"; 
-//		
-//		try {
-//			Files.copy(source.toPath(),(new File(path + dest.getName())).toPath(),StandardCopyOption.REPLACE_EXISTING);
-//		} catch (IOException e) {
-//			// TODO Auto-generated catch block
-//			e.printStackTrace();
-//		}
-//		
-//		//path is /Users/mouna/Desktop/test.java
-//
-//		 Result result = JUnitCore.runClasses(test.class);
-//	      for (Failure failure : result.getFailures()) {
-//	         System.out.println(failure.toString());
-//	      }
-//	      System.out.println(result.wasSuccessful());
-//	      if(result.wasSuccessful()){
-//	    	  System.out.println("******************************");
-//	 	      System.out.println("Test case passed for Version 1");
-//	 	      System.out.println("******************************");
-//	      }
 		
-	      System.out.println("Enter the URLS commits leading to the next version and type END when you're done");
-	      Scanner scanner = new Scanner(System.in);
+		String input = null;
+		System.out.println("Input the path of the test case"); 
+		try{
+			BufferedReader br = 
+	                      new BufferedReader(new InputStreamReader(System.in));
+				
+			input=br.readLine(); 
+				
+			if(input!=null){
+				System.out.println(input);
+				
+			}
+			
+			String s =""; 
+			Charset charset = Charset.forName("US-ASCII");
+			Path file=Paths.get(input); 
+			try (BufferedReader reader = Files.newBufferedReader(file, charset)) {
+			    String line = null;
+				FileWriter fw = new FileWriter("test.java");
+
+			    while ((line = reader.readLine()) != null) {
+			       
+					fw.write(line);
+					fw.write("\n");
+					
+			      
+			    }
+			    fw.close();
+			  
+
+			  
+			} catch (IOException x) {
+			    System.err.format("IOException: %s%n", x);
+			}
+			
+				
+		}catch(IOException io){
+			io.printStackTrace();
+		}	
+		
+		
+	          
+		File source = new File(input);
+		File dest = new File("/Users/mouna/Documents/Research/test/src/test/test.java");
+		 
+		String path="/Users/mouna/Documents/Research/test/src/test/"; 
+		
+		try {
+			Files.copy(source.toPath(),(new File(path + dest.getName())).toPath(),StandardCopyOption.REPLACE_EXISTING);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		//path is /Users/mouna/Desktop/test.java
+
+		 Result result = JUnitCore.runClasses(test.class);
+	      for (Failure failure : result.getFailures()) {
+	         System.out.println(failure.toString());
+	      }
+	      System.out.println(result.wasSuccessful());
+	      if(result.wasSuccessful()){
+	    	  System.out.println("******************************");
+	 	      System.out.println("Test case passed for Version 1");
+	 	      System.out.println("******************************");
+	      }
+		
+	    
+	      System.out.println("Enter the URLS of the next commits/versions and write END when you're done"); 
 	     
+	         try {
+	        	 Scanner scanner = new Scanner(System.in);
+	        	 BufferedWriter out = new BufferedWriter(new FileWriter("commits.txt"));
+	            
+	             while(scanner.hasNext()){
+	            	
+	            	 String s = scanner.next(); 
+	            	 if(s.equals("END")){
+	            		 break; 
+	            	 }
+	            	 System.out.println(s);
+	            	 out.write(s); 
+	            	 out.write("\n"); 
+	             }
+	             out.close();
+	           
+	            
+	         }
+	         catch (IOException e)
+	         {
+	             System.out.println("Exception ");       
+	         }
+
+
 	    	  
 	}
 	   
