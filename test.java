79b0cc28ee0b3dc4046d6f441ab5a2ba5a22347c
Mouna Hammoudi <mouna@cse.unl.edu>
2016-01-07 10:45:49 -0600

    NEW ATTEMPT FOR A NEW TEST

--- test.java
+++ test.java
@@ -1,4 +1,4 @@
-package test;
+package com.example.tests;
 
 import java.util.regex.Pattern;
 import java.util.concurrent.TimeUnit;
@@ -9,7 +9,7 @@ import org.openqa.selenium.*;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.support.ui.Select;
 
-public class test {
+public class TestTemp {
   private WebDriver driver;
   private String baseUrl;
   private boolean acceptNextAlert = true;
@@ -18,38 +18,36 @@ public class test {
   @Before
   public void setUp() throws Exception {
     driver = new FirefoxDriver();
-    baseUrl = "https://search.yahoo.com/";
-    driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+    baseUrl = "http://cse.unl.edu/";
+    driver.manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS);
   }
 
   @Test
-  public void test() throws Exception {
-    driver.get("http://cse.unl.edu/~mouna/WebApps/AddressBook/addressbookv1.2/index.php");
-    driver.findElement(By.linkText("add new")).click();
+  public void testTemp() throws Exception {
+    driver.get(baseUrl + "/~mouna/WebApps/AddressBook/addressbookv3.3.1/index.php");
+    driver.findElement(By.cssSelector("img[alt=\"Edit\"]")).click();
     driver.findElement(By.name("firstname")).clear();
-    driver.findElement(By.name("firstname")).sendKeys("James");
+    driver.findElement(By.name("firstname")).sendKeys("JamesEDIT");
     driver.findElement(By.name("lastname")).clear();
-    driver.findElement(By.name("lastname")).sendKeys("Pitt");
+    driver.findElement(By.name("lastname")).sendKeys("PittEDIT");
     driver.findElement(By.name("address")).clear();
-    driver.findElement(By.name("address")).sendKeys("44545 R street");
+    driver.findElement(By.name("address")).sendKeys("44545 R street EDIT");
     driver.findElement(By.name("home")).clear();
-    driver.findElement(By.name("home")).sendKeys("454545455");
+    driver.findElement(By.name("home")).sendKeys("454545455 8989");
     driver.findElement(By.name("mobile")).clear();
-    driver.findElement(By.name("mobile")).sendKeys("454554555");
+    driver.findElement(By.name("mobile")).sendKeys("454554555 2398");
     driver.findElement(By.name("work")).clear();
-    driver.findElement(By.name("work")).sendKeys("455455555");
+    driver.findElement(By.name("work")).sendKeys("455455555 32323");
     driver.findElement(By.name("email")).clear();
-    driver.findElement(By.name("email")).sendKeys("j.pitt@gmail.com");
+    driver.findElement(By.name("email")).sendKeys("j.pitt@gmail.com EDIT");
     driver.findElement(By.name("email2")).clear();
-    driver.findElement(By.name("email2")).sendKeys("jamespitt@hotmail.com");
-    new Select(driver.findElement(By.name("bday"))).selectByVisibleText("7");
-    new Select(driver.findElement(By.name("bmonth"))).selectByVisibleText("May");
-    driver.findElement(By.name("byear")).clear();
-    driver.findElement(By.name("byear")).sendKeys("1992");
-  
-   
-    driver.findElement(By.name("submit")).click();
-    driver.findElement(By.linkText("home")).click();
+    driver.findElement(By.name("email2")).sendKeys("jamespitt@hotmail.com EDIT");
+    driver.findElement(By.name("address2")).clear();
+    driver.findElement(By.name("address2")).sendKeys("EDIT");
+    driver.findElement(By.name("phone2")).clear();
+    driver.findElement(By.name("phone2")).sendKeys("EDIT");
+    driver.findElement(By.name("update")).click();
+    driver.findElement(By.linkText("Home")).click();
   }
 
   @After
